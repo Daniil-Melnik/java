9e84dc9a5b84710937926a8e3c291af40f32d11c
Daniil-Melnik <daniil.melnik.03@bk.ru>
2025-12-06 23:46:45 +0300

    added all of exeptions and assertions

--- src/ch_7/Main.java
+++ src/ch_7/Main.java
@@ -1,4 +1,4 @@
-package ch_7;
+//package ch_7;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -23,10 +23,22 @@ public class Main {
         // TestClass.TestChainOfExeptions("1");
         // TestClass.TestChainOfExeptions("");
 
-        try (var in = new Scanner(new FileInputStream("C:\\text\\1.txt"), StandardCharsets.UTF_8)){
+        /* try (var in = new Scanner(new FileInputStream("C:\\text\\1.txt"), StandardCharsets.UTF_8)){ // try с ресурсами
             while (in.hasNext()) System.out.println(in.next());
+            //Throwable t = new Throwable();
+            //t.printStackTrace(); // в стеке только этот main
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
+        StackWalker walker = StackWalker.getInstance();
+        walker.forEach(frame -> System.out.println(frame)); // в списке будет только main */
+
+        int a = 8; // проверка утверждений
+        //assert a <= 0;// assert условие;
+                        // закомментировать имя пакета
+                        // javac Main.java
+                        // java -ea Main
+
+        assert a <=0 : new AssertionError("Ошибка!!!"); // assert условие:выражение;
     }
 }
