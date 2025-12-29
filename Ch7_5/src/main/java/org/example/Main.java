3fd4286c9bc8289e67fccd9762a6b03b0d84a859
Daniil-Melnik <113520606+Daniil-Melnik@users.noreply.github.com>
2025-12-29 17:37:58 +0300

    -

---
+++ Ch7_5/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        // Logger.getGlobal().setLevel(Level.OFF); // OFF - запрет логирования в корне программы
+                                                   //
+        // Logger.getGlobal().info("test message"); // тестовая запись в консоль ИНФО
+
+        // продолжить appService1 по наследованию свойств протоколирования
+    }
+}
---
+++ Ch7_5/src/main/java/org/example/services/appService1.java
@@ -0,0 +1,10 @@
+package org.example.services;
+
+import java.util.logging.Logger;
+
+public class appService1 {
+    private static final Logger myLogger = Logger.getLogger("org.example.services");
+    public static void makeLog(){
+
+    }
+}
