9dfb747377996c62a57f912484a51045febdede0
Daniil-Melnik <113520606+Daniil-Melnik@users.noreply.github.com>
2025-12-30 15:54:48 +0300

    prep for 7.5.5

--- Ch7_5/src/main/java/org/example/Main.java
+++ Ch7_5/src/main/java/org/example/Main.java
@@ -1,18 +1,63 @@
 package org.example;
 
+import org.example.services.appService1;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    private static final Logger myLogger0 = Logger.getLogger("org.example");
+    private static final Logger myLogger1 = Logger.getLogger("org.example.services");
+
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        // ======================== элементарное протоколирование
+
         // Logger.getGlobal().setLevel(Level.OFF); // OFF - запрет логирования в корне программы
                                                    //
         // Logger.getGlobal().info("test message"); // тестовая запись в консоль ИНФО
 
-        // продолжить appService1 по наследованию свойств протоколирования
+        //======================== тестирование протоколирования разных уровней, точек выполнения entering
+
+        /* ConsoleHandler consoleHandler = new ConsoleHandler(); // определение кастомного обработчика
+        consoleHandler.setLevel(Level.FINEST); // опредление фильстра на стороне обработчика
+
+        myLogger1.setLevel(Level.FINEST); // определение фильтра на стороне регистратора
+        myLogger1.addHandler(consoleHandler); // использовать кастомный обработчик
+        myLogger1.setUseParentHandlers(false); // не использовать корневой обработчик
+
+        // см точку входа и выхода в методе класса
+
+        appService1.makeLog((myLogger, msgApp) -> {
+            myLogger.severe(String.format("severe msg from %s", msgApp));
+            myLogger.warning(String.format("warning msg from %s", msgApp));
+            myLogger.info(String.format("info msg from %s", msgApp));
+            myLogger.config(String.format("config msg from %s", msgApp));
+            myLogger.fine(String.format("fine msg from %s", msgApp));
+            myLogger.finer(String.format("finer msg from %s", msgApp));
+            myLogger.finest(String.format("finest msg from %s", msgApp));
+        }, myLogger1, "appService1"); */
+
+        //======================= тестирование интернационализации
+
+        // Logger logger = Logger.getLogger("org.example", "org.example.logmsg"); // указано имя комплекта ресурсов, берёт русскую из системы
+
+        // загрузка на другой локали
+
+        Locale enLocale = new Locale("en", "EN"); // описание локали для английского
+
+        ResourceBundle bundle = ResourceBundle.getBundle( // описаниение комплекта ресурсов под локаль enLocale
+                "org.example.logmsg", // имя комплекта
+                enLocale // указание локали для комплекта
+        );
+
+        Logger logger = Logger.getLogger("org.example"); // создание регистратора для пакета org.example
+        logger.setResourceBundle(bundle); // установка описанного выше комплекта на этот регистратор
+
+        logger.log(Level.INFO, "msg.info", "appServ"); // протокольная запись ИНФО сообщения по коду msg.info с добавкой appServ в {0}
     }
 }
--- Ch7_5/src/main/java/org/example/services/appService1.java
+++ Ch7_5/src/main/java/org/example/services/appService1.java
@@ -1,10 +1,14 @@
 package org.example.services;
 
+import java.util.function.BiConsumer;
+
 import java.util.logging.Logger;
 
 public class appService1 {
-    private static final Logger myLogger = Logger.getLogger("org.example.services");
-    public static void makeLog(){
-
+    public appService1(){}
+    public static void makeLog(BiConsumer<Logger, String> l, Logger logger, String msgAppend){
+        logger.entering(appService1.class.getName(), "makeLog");
+        l.accept(logger, msgAppend);
+        logger.exiting(appService1.class.getName(), "makeLog");
     }
 }
