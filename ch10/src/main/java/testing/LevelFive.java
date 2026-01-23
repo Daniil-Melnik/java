4d45f9297cc28fc1d7d02732000e3979266f486b
Daniil-Melnik <113520606+Daniil-Melnik@users.noreply.github.com>
2026-01-23 13:31:06 +0300

    LevelFive finished

--- ch10/src/main/java/testing/LevelFive.java
+++ ch10/src/main/java/testing/LevelFive.java
@@ -9,9 +9,6 @@
 - привязать к панели действия мыши: л-щелчок - квадрат, п-щелчок - окружность на место клика
 */
 
-// доделать окружности
-// ввести комментарии
-
 package testing;
 
 import javax.swing.*;
@@ -19,9 +16,9 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.geom.Ellipse2D;
-import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.util.LinkedList;
+import java.util.Objects;
 import java.util.Random;
 
 public class LevelFive {
@@ -41,70 +38,107 @@ class FrameFive extends JFrame{
     private static final int MARGIN_TOP = 5;
     private static final int MARGIN_LEFT = 5;
 
-    public FrameFive(int w, int h){
+    public FrameFive(int w, int h){ // фрейм приложения, принимает высоту и ширину
         setLayout(null);
         setSize(w, h);
-        addWindowListener(new WindowTerminator());
+        addWindowListener(new WindowTerminator()); // установка перехватчика оконных событий
+        setTitle("LevelFive");
+        setIconImage(new ImageIcon(
+                Objects.requireNonNull(
+                        this.getClass().getResource("/images/5.png")
+                )
+          ).getImage());
 
-        ComponentFive comp = new ComponentFive();
+        ComponentFive comp = new ComponentFive(); // введение в фрейм компонента для рисования фигур
         comp.setBounds(MARGIN_LEFT, MARGIN_TOP, w - MARGIN_LEFT*5, h - MARGIN_TOP*10);
-        comp.addMouseListener(new MouseClicker(comp));
+        comp.addMouseListener(new MouseClicker(comp)); // добавление к компоненту перехватчика событий от мыши
         add(comp);
     }
 }
 
-class ComponentFive extends JComponent{
-    private LinkedList<RectangleMod> rectangles;
+class ComponentFive extends JComponent{ // формирования компонента для отрисовки
+    private LinkedList<ShapeMod> shapes; // связный список спец. объектов (квадратов и окружностей) наносимых на компонент
 
     public ComponentFive(){
-        rectangles = new LinkedList<>();
+        shapes = new LinkedList<>();
     }
 
-    public void appendRectangle(double h, double w, Point2D p, Color c, boolean t){
-        rectangles.add(new RectangleMod(p.getX(), p.getY(), h, w, c, t));
+    public void appendShape(Shape s, boolean t){
+        shapes.add(new ShapeMod(s, t)); // метод для добавления фигуры в список фигур, все фигуры этого списка отрисовываются по циклу
     }
 
     @Override
-    protected void paintComponent(Graphics g) {
+    protected void paintComponent(Graphics g) { // отрисовка компонента
+        RectangleMod rect; // переменные для хранения рисуемого квадрата/окружности
+        EllipsMod ells;
+
         Graphics2D g2 = (Graphics2D) g;
         g2.setStroke(new BasicStroke(3));
         g2.setColor(Color.RED);
-        for (RectangleMod fO : rectangles){
-            g2.setColor(fO.getColor());
-            g2.draw(fO);
+        for (ShapeMod fO : shapes){ // перебор экземпляров квадратов/окружностей
+            if (!fO.getType()) { // если тип - false, то это квадрат
+                rect = (RectangleMod) fO.getShape(); // преобразование более общего Shape в оболочку для Rectangle2D
+                                                     // через приведение типов
+                ells = null; // переменная для окружности зануляется
+            }
+            else { // если тип фигуры - true, то это окружность
+                ells = (EllipsMod) fO.getShape(); // получение из более общего Shape кокретной оболочки для Ellipse2D
+                                                  // через приведение типов
+                rect = null; // параметр квадрата зануляется
+            }
+
+            // одна из фигур будет нулевой
+            g2.setColor(rect == null ? ells.getColor() : rect.getColor()); // установка цвета по ненулевой фигуре
+            g2.draw(rect == null ? ells : rect); // рисование ненулевой фигуры
         }
     }
 }
 
-class RectangleMod extends Rectangle2D.Double{
-    private Color color;
-    private boolean type;
+class ShapeMod{ // оболочка для хранения фигуры по более общему интерфейсу Shape
+    private Shape shape; // интерфейсный параметр Shape, пригоден для хранения расширений Rectangle2D и Ellipse2D
+    private boolean type; // параметр типа фигуры true - окружность
+                          //                      false - квадрат
 
-    public RectangleMod(double h, double w, double x, double y, Color c, boolean t){
-        super(h, w, x, y);
-        color = c;
+    public ShapeMod(Shape s, boolean t){
+        shape = s;
         type = t;
     }
 
-    public RectangleMod(){
-        super();
-        color = Color.BLACK;
-        type = false;
-    }
-
-    public Color getColor(){
-        return color;
+    public ShapeMod(){
+        this(null, false);
     }
 
     public boolean getType(){
         return type;
     }
+
+    public Shape getShape() {
+        return shape;
+    }
 }
 
-class EllipsMod extends Ellipse2D.Double{
+class RectangleMod extends Rectangle2D.Double{ // оболочка для хранения Rectangle2D.Double вместе с цветом
+    private Color color; // параметр цвета
+
+    public RectangleMod(double x, double y, double h, double w, Color c){
+        super(h, w, x, y);
+        color = c;
+    }
+
+    public RectangleMod(){
+        super();
+        color = Color.BLACK;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+}
+
+class EllipsMod extends Ellipse2D.Double{ // оболочка для хранения Ellipse2D.Double вместе с цветом
     private Color color;
 
-    public EllipsMod(double h, double w, double x, double y, Color c){
+    public EllipsMod(double x, double y, double h, double w, Color c){
         super(h, w, x, y);
         color = c;
     }
@@ -113,11 +147,15 @@ class EllipsMod extends Ellipse2D.Double{
         super();
         color = Color.BLACK;
     }
+
+    public Color getColor(){
+        return color;
+    }
 }
 
-class MouseClicker extends MouseAdapter{
+class MouseClicker extends MouseAdapter{ // класс перехватчика событий мыши на основе адаптера
 
-    private ComponentFive component;
+    private ComponentFive component; // параметр, содержащий ссылку на компонент, для которого был создан
 
     public MouseClicker(ComponentFive c){
         component = c;
@@ -131,20 +169,22 @@ class MouseClicker extends MouseAdapter{
         int iG = Math.abs((e.getX() + e.getY() + 200) % 255);
         int iB = Math.abs((e.getX() + e.getY()) % 255);
 
-        Color randColor = new Color(iR, iG, iB);
+        Color randColor = new Color(iR, iG, iB); // создание соответсвующего цвета
 
-        switch (e.getButton()){
-            case 1:
-                component.appendRectangle(10, 10, e.getPoint(), randColor, false);
+        switch (e.getButton()){ // разбиение по нажатой кнопке мыши
+            case 1: // нажата ЛКМ - добавить квадрат
+                component.appendShape(
+                        new RectangleMod(10, 10, e.getX(), e.getY(), randColor), false); // добавление квадрата в список фигур компонента
                 break;
-            case 3:
-                component.appendRectangle(10, 10, e.getPoint(), randColor, true);
+            case 3: // нажата ПКМ - добавить окружность
+                component.appendShape(
+                        new EllipsMod(10, 10, e.getX(), e.getY(), randColor), true); // добавление окружности в список фигур компонента
                 break;
-            default:
+            default: // по умолчанию (СКМ не учитывается) - вывести отладочное сообщение
                 System.out.println("middle pressed");
                 break;
         }
 
-        component.repaint();
+        component.repaint(); // перерисовка компонента
     }
 }
