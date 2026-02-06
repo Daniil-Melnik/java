aed163c3e56b2d06e039630c43e5eb5c189324eb
Daniil-Melnik <113520606+Daniil-Melnik@users.noreply.github.com>
2026-02-06 17:35:11 +0300

    -

--- ch11/src/main/java/testing/LevelThree.java
+++ ch11/src/main/java/testing/LevelThree.java
@@ -6,7 +6,50 @@
 - добавить строку меню с возможностью генерации новой обстановки, загрузки и сохранения в файл
 */
 
+// добавить кнопки и логику
+
 package testing;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class LevelThree {
+    private static final int FRAME_H = 400;
+    private static final int FRAME_W = 360;
+
+    private static final int GAME_PAN_H = 360;
+    private static final int GAME_PAN_W = 360;
+
+    private static MainFrame mainFrame;
+    private static GamePanel gamePanel;
+
+    public static void main(String... args){
+        EventQueue.invokeLater(() -> {
+            mainFrame = new MainFrame();
+            mainFrame.setVisible(true);
+        });
+    }
+
+    private static class MainFrame extends JFrame{
+        public MainFrame(){
+            setLayout(new BorderLayout());
+            setDefaultCloseOperation(EXIT_ON_CLOSE);
+            setSize(FRAME_W, FRAME_H);
+
+            gamePanel = new GamePanel();
+            add(gamePanel, BorderLayout.SOUTH);
+        }
+    }
+
+    private static class GamePanel extends JPanel{
+        public GamePanel(){
+            setLayout(new GridLayout());
+            setBackground(Color.GREEN);
+        }
+
+        @Override
+        public Dimension getPreferredSize() {
+            return new Dimension(GAME_PAN_H, GAME_PAN_W);
+        }
+    }
 }
