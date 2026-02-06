33e770bfed350f33c2a8c043e7f76f145dbe57e8
Daniil-Melnik <daniil.melnik.03@bk.ru>
2026-02-06 23:35:44 +0300

    started GameLogic

--- ch11/src/main/java/testing/LevelThree.java
+++ ch11/src/main/java/testing/LevelThree.java
@@ -12,6 +12,10 @@ package testing;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 public class LevelThree {
     private static final int FRAME_H = 400;
@@ -30,6 +34,8 @@ public class LevelThree {
         });
     }
 
+
+
     private static class MainFrame extends JFrame{
         public MainFrame(){
             setLayout(new BorderLayout());
@@ -42,6 +48,7 @@ public class LevelThree {
     }
 
     private static class GamePanel extends JPanel{
+
         public GamePanel(){
             setLayout(new GridLayout());
             setBackground(Color.GREEN);
@@ -52,4 +59,53 @@ public class LevelThree {
             return new Dimension(GAME_PAN_H, GAME_PAN_W);
         }
     }
+
+    private static class GameButton extends JButton{
+        private int posX;
+        private int posY;
+
+        public GameButton(int x, int y, String val){
+            super(val);
+            posX = x;
+            posY = y;
+        }
+
+        public void setPosition(int nX, int nY){
+            posY = nY;
+            posX = nX;
+        }
+
+        public String getVal(){
+            return super.getText();
+        }
+    }
+
+    private static class GameLogic{
+        private String[] field = {"3", "6", "1", "4", "5", "2", "7", "8", "*"};
+        private HashMap<String, HashSet<String>> enabledPositions = new HashMap<>();
+        private ArrayList<String> currentEnPos;
+        private int zeroIndex = 8;
+
+        public GameLogic(){
+            enabledPositions.put("0", new HashSet<>(List.of("1", "3")));
+            enabledPositions.put("1", new HashSet<>(List.of("0", "2", "4")));
+            enabledPositions.put("2", new HashSet<>(List.of("1", "5")));
+            enabledPositions.put("3", new HashSet<>(List.of("0", "4", "6")));
+            enabledPositions.put("4", new HashSet<>(List.of("1", "3", "5", "7")));
+            enabledPositions.put("5", new HashSet<>(List.of("8", "4", "2")));
+            enabledPositions.put("6", new HashSet<>(List.of("3", "7")));
+            enabledPositions.put("7", new HashSet<>(List.of("6", "4", "8")));
+            enabledPositions.put("8", new HashSet<>(List.of("7", "5")));
+        }
+
+        public void move(){
+
+        }
+
+        public String[] getField(){
+            return field;
+        }
+    }
+
+
 }
