55cf4e9fcdb136f3bd49d1364da9b581b79e54cc
Daniil-Melnik <113520606+Daniil-Melnik@users.noreply.github.com>
2026-02-10 18:46:04 +0300

    added menu

--- ch11/src/main/java/testing/LevelFour.java
+++ ch11/src/main/java/testing/LevelFour.java
@@ -15,12 +15,19 @@ package testing;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.util.Objects;
 
 public class LevelFour {
     private static MainFrame mainFrame;
+    private static TextPanel textPanel;
+    private static TextComponent textComponent;
+
     private static final int FRAME_W = 800;
     private static final int FRAME_H = 500;
+
+    private static final int TEXT_PANEL_W = 800;
+    private static final int TEXT_PANEL_H = 350;
 
     public static void main(String... args){
         EventQueue.invokeLater(() -> {
@@ -35,10 +42,91 @@ public class LevelFour {
             setDefaultCloseOperation(EXIT_ON_CLOSE);
             setResizable(false);
             setTitle("Просмотр текста");
+            setLayout(new BorderLayout());
             setIconImage(
                     new ImageIcon(
                             Objects.requireNonNull(this.getClass().getResource("/text.png"))
                     ).getImage());
+
+            textPanel = new TextPanel();
+
+            add(textPanel, BorderLayout.NORTH);
+
+            setJMenuBar(new MainMenuBar());
+        }
+    }
+
+    private static class TextPanel extends JPanel{
+        public TextPanel(){
+            setBackground(Color.WHITE);
+            setLayout(new BorderLayout());
+            textComponent = new TextComponent();
+
+            add(textComponent, BorderLayout.CENTER);
+        }
+
+        @Override
+        public Dimension getPreferredSize() {
+            return new Dimension(TEXT_PANEL_W, TEXT_PANEL_H);
+        }
+    }
+
+    private static class TextComponent extends JComponent{
+        private String text = "Hello, it's text component";
+        private Font font = new Font("Arial", Font.PLAIN, 16);
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            g.setFont(font);
+
+            FontMetrics fm = getFontMetrics(font);
+            int strWidth = fm.stringWidth(text);
+            int strHeight = fm.getHeight();
+
+            g.drawString(text, (TEXT_PANEL_W - strWidth) / 2,(TEXT_PANEL_H - strHeight) / 2);
+        }
+
+        @Override
+        public void setFont(Font f) {
+            font = f;
+        }
+
+        public void setText(String t) {
+            text = t;
+        }
+
+        public void setComponent(String t, Font f){
+            setText(t);
+            setFont(f);
+        }
+    }
+
+    /*
+    - Файл
+    - Содержание (кастомное диалоговое окно)
+    - Шрифт
+      -- гарнитура
+      -- начертание
+    - цвет
+      -- наборный
+      -- константный
+
+    размер - элемент на панели
+
+    панель с информацией от текущих настройках + размеры строки в пикселях
+    */
+
+    private static class MainMenuBar extends JMenuBar{
+        public MainMenuBar(){
+            JMenu fileMenu = new JMenu("Файл");
+            JMenu textMenu = new JMenu("Содержание");
+            JMenu fontMenu = new JMenu("Шрифт");
+            JMenu colorMenu = new JMenu("Цвет");
+
+            add(fileMenu);
+            add(textMenu);
+            add(fontMenu);
+            add(colorMenu);
         }
     }
 }
