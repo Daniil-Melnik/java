31c1bc1061283d9506897b75635b824407dab70d
Daniil-Melnik <113520606+Daniil-Melnik@users.noreply.github.com>
2026-02-10 16:50:59 +0300

    added MainFrame to lvl4

--- ch11/src/main/java/testing/LevelFour.java
+++ ch11/src/main/java/testing/LevelFour.java
@@ -13,6 +13,32 @@
 
 package testing;
 
+import javax.swing.*;
+import java.awt.*;
+import java.util.Objects;
+
 public class LevelFour {
-    
+    private static MainFrame mainFrame;
+    private static final int FRAME_W = 800;
+    private static final int FRAME_H = 500;
+
+    public static void main(String... args){
+        EventQueue.invokeLater(() -> {
+            mainFrame = new MainFrame();
+            mainFrame.setVisible(true);
+        });
+    }
+
+    private static class MainFrame extends JFrame{
+        public MainFrame(){
+            setSize(FRAME_W, FRAME_H);
+            setDefaultCloseOperation(EXIT_ON_CLOSE);
+            setResizable(false);
+            setTitle("Просмотр текста");
+            setIconImage(
+                    new ImageIcon(
+                            Objects.requireNonNull(this.getClass().getResource("/text.png"))
+                    ).getImage());
+        }
+    }
 }
