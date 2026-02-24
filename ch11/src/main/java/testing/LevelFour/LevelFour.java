68b5715526798a6eb80eee60ef3e45f9c8e2e85b
Daniil-Melnik <daniil.melnik.03@bk.ru>
2026-02-24 20:19:05 +0300

    Comments for LevelFour complete

--- ch11/src/main/java/testing/LevelFour/LevelFour.java
+++ ch11/src/main/java/testing/LevelFour/LevelFour.java
@@ -3,16 +3,13 @@
 - сформировать фрейм, поместить в него компонент с текстом
 - добавить в фрейм панели с выбором: шрифта, начертания, размера
   -- шрифт - компобокс
-  -- размер - регулируемый ползунок
-  -- начертание - чекбоксы
+  -- размер - комбобокс
+  -- начертание - радио-кнопки
   -- цвет - меню радио кнопок - наборный через комбо
                               - из констант через компбо
   -- изменение текста через поля для ввода
 - все панели изменений провести через строку меню
 */
-
-// 0 добавить панель с информацией о шрифте (ширина + высота)
-// добавлено взятие метрик из компонента, осталась отрисовка панели
 
 package testing.LevelFour;
 
@@ -30,12 +27,12 @@ import java.io.*;
 import java.util.*;
 
 public class LevelFour {
-    private static MainFrame mainFrame;
-    private static TextComponent textComponent;
-    private static BottomPanel bottomPanel;
-    private static TextInfoPanel textInfoPanel;
+    private static MainFrame mainFrame; // главное окно
+    private static TextComponent textComponent; // копонент с тестовой строкой
+    private static BottomPanel bottomPanel; // нижняя панель (строка ввода, панель с метриками)
+    private static TextInfoPanel textInfoPanel; // панель с метриками
 
-    private static final int FRAME_W = 800;
+    private static final int FRAME_W = 800; // константы размеров окна и панелей
     private static final int FRAME_H = 400;
 
     private static final int TEXT_PANEL_W = 800;
@@ -62,21 +59,22 @@ public class LevelFour {
                             Objects.requireNonNull(this.getClass().getResource("/text.png"))
                     ).getImage());
 
-            TextPanel textPanel = new TextPanel();
-            bottomPanel = new BottomPanel();
+            TextPanel textPanel = new TextPanel(); // инициализация панели под текстовых компонент
+            bottomPanel = new BottomPanel(); // инициализация панели под строку поиска и панель с метриками
 
-            add(textPanel, BorderLayout.CENTER);
+            add(textPanel, BorderLayout.CENTER); // именно CENTER (а не NORTH) - занимает только централиьную область
+                                                 // и не нарушает панель SOUTH
             add(bottomPanel, BorderLayout.SOUTH);
 
-            setJMenuBar(new MainMenuBar());
+            setJMenuBar(new MainMenuBar()); // установка панели пеню
         }
     }
 
-    private static class TextPanel extends JPanel{
+    private static class TextPanel extends JPanel{ // панель под текстовый компонент
         public TextPanel(){
             setLayout(new BorderLayout());
             setBackground(Color.WHITE);
-            textComponent = new TextComponent();
+            textComponent = new TextComponent(); // инициализация текстового компонента
 
             add(textComponent, BorderLayout.CENTER);
         }
@@ -87,39 +85,40 @@ public class LevelFour {
         }
     }
 
-    private static class TextComponent extends JComponent{
-        private String text = "Hello, it's text component";
-        private String fontName = "Arial";
-        private int fontSize = 26;
-        private int fontOutline = Font.BOLD;
-        private Font font;
-        private Color color = new Color(100, 100, 100);
+    private static class TextComponent extends JComponent{ // текстовый компонент,  содержит информацияю о
+        private String text = "Hello, it's text component"; // тексте, который отображает компонент
+        private String fontName = "Arial"; // имени гарнитуры надписи
+        private int fontSize = 26; // размер шрифта текста
+        private int fontOutline = Font.BOLD; // начертание шрифта
+        private Font font; // инкапсулированное поле шрифта
+        private Color color = new Color(100, 100, 100); // цвет написания ятекста
 
         public TextComponent(){
             font = new Font(fontName, fontOutline, fontSize);
         }
 
         @Override
-        protected void paintComponent(Graphics g) {
+        protected void paintComponent(Graphics g) { // отрисовка компонента
             g.setFont(font);
             g.setColor(color);
 
-            FontMetrics fm = getFontMetrics(font);
-            int strWidth = fm.stringWidth(text);
-            int strHeight = fm.getHeight();
+            FontMetrics fm = getFontMetrics(font); // объект для расчета размеров строки текста в заданном шрифте
+                                                   // расчет координа ЛВ-угла чтобы строка была примерно по центру
+            int strWidth = fm.stringWidth(text); // ширина строки
+            int strHeight = fm.getHeight();  // высота строки
 
             g.drawString(text, (TEXT_PANEL_W - strWidth) / 2,(TEXT_PANEL_H - strHeight) / 2);
         }
 
         public FontMetrics getFM(){
             return getFontMetrics(font);
-        }
+        }  // быстрое получение экземпляра FontMetrics
 
-        public void setFont() {
+        public void setFont() { // установка нового шрифта (при изменении через интерфейс параметров)
             Font newFont = new Font(fontName, fontOutline, fontSize);
-            if (getFontMetrics(newFont).stringWidth(text) <= MAX_TEXT_W){
+            if (getFontMetrics(newFont).stringWidth(text) <= MAX_TEXT_W){ // проверка: помещается ли строка на экран
                 font = newFont;
-            } else {
+            } else { // если слишком длинная - не обновлять шрифт
                 JOptionPane.showMessageDialog(mainFrame, "Слишком длинная строка, уменьшите пожалуйста её длину", "Длинная строка", JOptionPane.ERROR_MESSAGE);
             }
         }
@@ -136,26 +135,20 @@ public class LevelFour {
             fontSize = fS;
         }
 
-        public void setText(String t) throws BadLocationException {
+        public void setText(String t) throws BadLocationException { // установка хранимого текста
             FontMetrics fm = getFontMetrics(font);
-
+            // проверка: помещается ли строка на панель
+            // если нет - остановка возможности ввода новых символов: установка нового модифицированного Document
+            // с ограничением длины вводимой строки и установкой введённого текста
+            // получится только удалить ссимвол перед вводом нового
             if (fm.stringWidth(t) > MAX_TEXT_W){
                 bottomPanel.getInputTextField().setDocument(new LimitDocument(t.length() - 1));
                 bottomPanel.getInputTextField().setText(text);
             }
-            else{
+            else{ // если текст помещается: обновляется ограничение доины в Document
                 text = t;
                 ((LimitDocument) bottomPanel.getInputTextField().getDocument()).updateDocument(text.length() + 1);
             }
-        }
-
-        public String getText(){
-            return text;
-        }
-
-        public void setComponent(String t, Font f) throws BadLocationException {
-            setText(t);
-            setFont(f);
         }
 
         public void setColor(Color c) {
@@ -166,7 +159,8 @@ public class LevelFour {
             return color;
         }
 
-        public LinkedHashMap<String, String> getFontInfo(){
+        public LinkedHashMap<String, String> getFontInfo(){ // метод для формирования информации о настройках шрифта (для
+                                                            // панели метрик)
             LinkedHashMap<String, String> info = new LinkedHashMap<>(4);
             info.put("fontName", fontName);
             info.put("fontOutline", fontOutline == Font.BOLD ? "Bold" : fontOutline == Font.PLAIN ? "Plain" : fontOutline == Font.ITALIC ? "Italic" : "ERROR");
@@ -175,7 +169,7 @@ public class LevelFour {
             return info;
         }
 
-        public LinkedHashMap<String, String> getStringMetrics(){
+        public LinkedHashMap<String, String> getStringMetrics(){ // метод формирования метрик строки (для панели метрик)
             LinkedHashMap<String, String> metrics = new LinkedHashMap<>(2);
             metrics.put("strWidth", Integer.toString(getFM().stringWidth(text)));
             metrics.put("strHeight", Integer.toString(getFM().getHeight()));
@@ -187,14 +181,13 @@ public class LevelFour {
         private JTextField inputTextField;
         private LimitDocument limitDocument;
 
-        public BottomPanel(){
+        public BottomPanel(){ // нижня панель вкючает в себя строку ввода и панель метрик
             limitDocument = new LimitDocument(100);
             setBorder(new EtchedBorder());
             setLayout(new GridBagLayout());
-            inputTextField = new JTextField();
+
+            inputTextField = new JTextField(); // поле для ввода строки
             inputTextField.setFont(new Font("Arial", Font.PLAIN, 20));
-            
-            inputTextField.setText("Hello, it's text component");
             inputTextField.setDocument(limitDocument);
 
             JLabel textLabel = new JLabel("Строка:");
@@ -203,14 +196,14 @@ public class LevelFour {
             add(textLabel, new GBC(0, 0, 2, 1).setWeight(0, 0.1).setAnchor(GBC.EAST).setInsets(0, 0, 10, 0));
             add(inputTextField, new GBC(2, 0, 14, 1).setWeight(1, 0.1).setFill(1).setInsets(10));
 
-            textInfoPanel = new TextInfoPanel();
+            textInfoPanel = new TextInfoPanel(); // панель с метриками о шрифте и строке
 
             add(textInfoPanel, new GBC(0, 1, 16, 2).setWeight(1, 1).setFill(1));
         }
 
         public JTextField getInputTextField(){
             return inputTextField;
-        }
+        } // доступ к полю ввода строки
 
         @Override
         public Dimension getPreferredSize() {
@@ -218,15 +211,15 @@ public class LevelFour {
         }
     }
 
-    private static class TextInfoPanel extends JPanel{
+    private static class TextInfoPanel extends JPanel{ // панель с метриками
         private static final Font font12 = new Font("Arial", Font.BOLD, 12);
 
-        LinkedHashMap<String, JLabel> valuesMap = new LinkedHashMap<>(4);
-        LinkedHashMap<String, JLabel> metricsMap = new LinkedHashMap<>();
-        Map<String, JLabel> labelsMap;
+        LinkedHashMap<String, JLabel> valuesMap = new LinkedHashMap<>(4); // отображение метрик шрифта
+        LinkedHashMap<String, JLabel> metricsMap = new LinkedHashMap<>(2); // отображение метрик строки
+        Map<String, JLabel> labelsMap; // отображения лейблов для метрик
         Map<String, JLabel> labelsMetrMap;
 
-        {
+        { // блок инициализации для отображений пометок для метрик
             labelsMap = Map.of(
                     "fontName", new JLabel("Название:"),
                     "fontOutline", new JLabel("Начертание:"),
@@ -249,6 +242,7 @@ public class LevelFour {
             fontInfoPanel.setLayout(new GridBagLayout());
             fontMetricsPanel.setLayout(new GridBagLayout());
 
+            // первичное заполнение значений метрик yа основе методов текстового компонента и предславлений (k, v)
             for (Map.Entry<String, String> e : textComponent.getFontInfo().entrySet()){
                 valuesMap.put(e.getKey(), new JLabel(e.getValue()));
             }
@@ -257,6 +251,7 @@ public class LevelFour {
                 metricsMap.put(e.getKey(), new JLabel(e.getValue()));
             }
 
+            // нанесение на панель метрик шрифта подписей и значений
             int i = 0;
             for (String s : labelsMap.keySet()){
                 labelsMap.get(s).setFont(font12);
@@ -276,6 +271,7 @@ public class LevelFour {
 
             i = 0;
 
+            // нанесение на панель метрик строки подписей и значений
             for (String s : metricsMap.keySet()){
                 fontMetricsPanel.add(labelsMetrMap.get(s),
                         new GBC(0, i, 1, 1)
@@ -290,7 +286,7 @@ public class LevelFour {
                 i++;
             }
 
-            fontMetricsPanel.setBorder(new EtchedBorder());
+            fontMetricsPanel.setBorder(new EtchedBorder()); // ограничение панелей
             fontInfoPanel.setBorder(new EtchedBorder());
 
             add(fontInfoPanel);
@@ -298,7 +294,7 @@ public class LevelFour {
             add(fontMetricsPanel);
         }
 
-        public void updatePanel(){
+        public void updatePanel(){ // обновление панели = обновление значений в соответствующих отображениях
             for (Map.Entry<String, String> e : textComponent.getFontInfo().entrySet()){
                 valuesMap.get(e.getKey()).setText(e.getValue());
             }
@@ -310,23 +306,23 @@ public class LevelFour {
         }
     }
 
-    private static class MainMenuBar extends JMenuBar{
+    private static class MainMenuBar extends JMenuBar{ // панель меню
         private static final boolean PALETTE = false;
         private static final boolean ADDITIVE = true;
 
         public MainMenuBar(){
-            JMenu fileMenu = new JMenu("Файл (F)");
+            JMenu fileMenu = new JMenu("Файл (F)"); // к каждому меню присвоена мнемоника
             JMenu fontMenu = new JMenu("Шрифт (S)");
             JMenu colorMenu = new JMenu("Цвет (C)");
 
             JMenu outlineMenu = new JMenu("Начертание (O)");
 
-            outlineMenu.setMnemonic(KeyEvent.VK_O);
+            outlineMenu.setMnemonic(KeyEvent.VK_O); // установка мнемоник к меню
             fileMenu.setMnemonic(KeyEvent.VK_F);
             fontMenu.setMnemonic(KeyEvent.VK_S);
             colorMenu.setMnemonic(KeyEvent.VK_C);
 
-            JMenuItem exitItem = new JMenuItem("Выход (E)", 'E');
+            JMenuItem exitItem = new JMenuItem("Выход (E)", 'E'); // конструирование пунктов меню с мнемониками
             JMenuItem aboutItem = new JMenuItem("О программе (A)", 'A');
 
             exitItem.addActionListener((e) -> {System.exit(0);});
@@ -342,7 +338,8 @@ public class LevelFour {
             fileMenu.add(aboutItem);
 
             JMenuItem shriftItem = new JMenuItem("Гарнитура (G)", 'G');
-            ButtonGroup radioGroup = new ButtonGroup();
+
+            ButtonGroup radioGroup = new ButtonGroup(); // набор из 3-х радио-кнопок под выбор начертания
             JRadioButtonMenuItem outlineItemPlain = new JRadioButtonMenuItem(new OutlineAction("Plain"));
             JRadioButtonMenuItem outlineItemBold = new JRadioButtonMenuItem(new OutlineAction("Bold"));
             JRadioButtonMenuItem outlineItemItalic = new JRadioButtonMenuItem(new OutlineAction("Italic"));
@@ -355,11 +352,11 @@ public class LevelFour {
             outlineMenu.add(outlineItemItalic);
 
             shriftItem.addActionListener((e) -> {
-                try {
+                try { // получение диалогового окна выбора гарнитуры
                     DialogWindows.TextAdder tA = DialogWindows.getTextAddedDialog(mainFrame);
-                    if (tA.showDialog()){
-                        textComponent.setFontName(tA.getText());
-                        textComponent.setFont();
+                    if (tA.showDialog()){ // если в диалоге нажата "ок" - возврат true => можно:
+                        textComponent.setFontName(tA.getText()); // получить имя текста по методу из диалога
+                        textComponent.setFont(); // обновить шрифт в компоненте и обновить сам компонент
                         textComponent.repaint();
                         textInfoPanel.updatePanel();
                     }
@@ -370,18 +367,18 @@ public class LevelFour {
             JMenuItem colorAddsItem = new JMenuItem("Наборный (N)", 'N');
             JMenuItem colorPaletteItem = new JMenuItem("Плитка (P)", 'P');
 
-            colorAddsItem.addActionListener((e) -> {
+            colorAddsItem.addActionListener((e) -> { // наборное диалоговое окно выбора текста
                 DialogWindows.TextColorChooser tC = DialogWindows.getTextColorChooser(mainFrame, ADDITIVE, textComponent.getColor());
-                if (tC.showDialog()){
-                    textComponent.setColor(tC.getColor());
-                    textComponent.repaint();
+                if (tC.showDialog()){ // нажата "ок" =>
+                    textComponent.setColor(tC.getColor()); // получить из диалога выбранный цвет
+                    textComponent.repaint(); // обновить компонент со строкой
                     textInfoPanel.updatePanel();
                 }
             });
 
-            colorPaletteItem.addActionListener((e) -> {
+            colorPaletteItem.addActionListener((e) -> { // плиточное диалоговое окно выбора текста
                 DialogWindows.TextColorChooser tC = DialogWindows.getTextColorChooser(mainFrame, PALETTE, textComponent.getColor());
-                if (tC.showDialog()){
+                if (tC.showDialog()){ // аналогично выше
                     textComponent.setColor(tC.getColor());
                     textComponent.repaint();
                     textInfoPanel.updatePanel();
@@ -389,10 +386,10 @@ public class LevelFour {
             });
 
             JMenuItem textSizeItem = new JMenuItem("Размер (S)", 'S');
-            textSizeItem.addActionListener((e) -> {
+            textSizeItem.addActionListener((e) -> { // диалоговое окно выбора размера текста
                 DialogWindows.TextSizeChooser tSC = DialogWindows.getTextSizeChooser(mainFrame);
-                if (tSC.showDialog()){
-                    textComponent.setFontSize(tSC.getSizeFromCombo());
+                if (tSC.showDialog()){ // нажата "ок" =>
+                    textComponent.setFontSize(tSC.getSizeFromCombo()); // получить выбранный размер из диалога
                     textComponent.setFont();
                     textComponent.repaint();
                     textInfoPanel.updatePanel();
@@ -434,13 +431,14 @@ public class LevelFour {
         }
     }
 
+    // модифицированный Document (модель текстового компонента по MVC)
     private static class LimitDocument extends PlainDocument{
-        private int limit;
+        private int limit; // ограничение длины строки в символах
 
         public LimitDocument(int l){
             super();
             limit = l;
-            addDocumentListener(new DocumentListener() {
+            addDocumentListener(new DocumentListener() { // здесь же можно добавить перехватчик обновления строки
                 @Override
                 public void insertUpdate(DocumentEvent e) {
                     try {
@@ -477,7 +475,7 @@ public class LevelFour {
         public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {
             if (str == null) return;
 
-            if ((getLength() + str.length() <= limit)){
+            if ((getLength() + str.length() <= limit)){ // переопределение метода вставки строки с учетом ограничения длины
                 super.insertString(offs, str, a);
             }
         }
